8f63d2015289b8f19cc14353ef04aa7c0c7a3431
AdhikarShinde <adhikar.shinde@coditas.com>
2021-11-24 13:25:59 +0530

    Added Structure to login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router";
+import styles from "../Login/Login.module.scss"
 
 interface ILogin {
   email: String;
@@ -11,13 +12,14 @@ export default function App() {
   const { register, handleSubmit } = useForm<ILogin>();
   const onSubmit: SubmitHandler<ILogin> = data => navigation("dashboard");
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+  return (<div className={styles.login} >
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <label>Email</label>
       <input {...register("email")} />
       <label>Password</label>
-      <input {...register("password")} />
+      <input {...register("password")} /><br/>
       <input type="submit" />
     </form>
+    </div>
   );
 }
